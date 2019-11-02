d48a9976ce7b44c2cb873d9c12db8ab7b115b9bd
taylor-misch <taylormisch@gmail.com>
2019-11-02 16:40:25 -0700

    cards are placed at the proper part of the scene

--- Assets/Scripts/PlayMat.cs
+++ Assets/Scripts/PlayMat.cs
@@ -48,15 +48,17 @@ public class PlayMat : MonoBehaviour
             
             // Reparent the card under the hand object.
             card.go.transform.parent = handToUse.go.transform;
+            card.go.transform.rotation = isRed ? Quaternion.Euler(-33.44f, 0f,0f) : Quaternion.Euler(33.44f, 0f,0f) ;
             // TODO Taylor fix height problem
-            card.go.transform.position = handToUse.go.transform.position;
+            card.go.transform.position = handToUse.go.transform.position + (isRed ? new Vector3(-1.9f+(i*.75f), 0,0) : new Vector3(-1.9f+(i*.75f), 0,0));
+            card.go.transform.localScale -= new Vector3(.3f, .3f, 0);
         }
     }
 
     private Hand SpawnHand(bool isRed) {
         // Spawn Hand
-        var handGo = Instantiate(handPrefab, (isRed ? new Vector3(-2, 0, -2) : new Vector3(5, 0, 5)),
-                                Quaternion.identity, playMat.transform);
+        var handGo = Instantiate(handPrefab, (isRed ? new Vector3(0, .91f, 3f) : new Vector3(0, .61f, -4.5f)),
+                               Quaternion.identity, playMat.transform);
         handGo.name = isRed ? "RedHand" : "BlackHand";
         Hand hand = handGo.GetComponent<Hand>();
         hand.go = handGo;
@@ -77,7 +79,7 @@ public class PlayMat : MonoBehaviour
         
         for (int i = 2; i <= 14; i++) {
             var cardGo = GameObject.Instantiate(cardPrefab,
-                (isRed ? Vector3.zero : new Vector3(2, 0, 2)) + new Vector3(0, 0, i * .01f),
+                (isRed ? new Vector3(-4.01f, 0.1f-.3f, 3.5f) : new Vector3(3.87f, 0.1f-.3f, -3.8f)) + new Vector3(0, i * .05f, 0),
                 cardPrefab.transform.rotation, deckGo.transform);
             Card card = cardGo.GetComponent<Card>();
             card.value = i;
@@ -90,7 +92,7 @@ public class PlayMat : MonoBehaviour
 
             // Create second card for same value
             var cardGo2 = GameObject.Instantiate(cardPrefab,
-                (isRed ? Vector3.zero : new Vector3(2, 0, 2)) + new Vector3(0, 0, i * .01f),
+                (isRed ? new Vector3(-4.01f, 0.1f, 3.5f) : new Vector3(3.87f, 0.1f, -3.8f)) + new Vector3(0, cardGo.transform.position.y + .025f-0.09f, 0),
                 cardPrefab.transform.rotation, deckGo.transform);
             Card card2 = cardGo2.GetComponent<Card>();
             card2.value = i;
