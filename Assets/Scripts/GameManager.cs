643b611f4e970836b3df2950b9f72f9523a250c2
Ishmael <jaswged@gmail.com>
2019-11-02 12:40:40 -0500

    Created game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player {
+    public static GameManager instance;
+    private Camera mainCamera;
+	
+    private Player red;
+    private Player black;
+    [SerializeField]
+	private Player currentPlayer;
+
+    void Awake() {
+        instance = this;
+        mainCamera = Camera.main;
+    }
+
+	void Start () {
+        red = new Player("red", true);
+        black = new Player("black", false);
+
+		// TODO Create player method that creates deck and such?
+        currentPlayer = white;
+        otherPlayer = black;
+
+        InitialSetup();
+    }
+
+	private void InitialSetup() {
+		// TODO Spawn decks
+
+		// Draw hands
+
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player {
+	// TODO replace with Deck object
+	public List<GameObject> deck;
+
+	public GameObject hand;
+
+	private bool isAi;
+	public string Name;
+	
+	public Player(string name, bool isAi) {
+		this.Name = name;
+		this.isAi = isAi;
+		
+		deck = new List<GameObject>();
+	}
+}
