9ca60d790d23e316654d1c4c73a562f5135cad52
taylor-misch <taylormisch@gmail.com>
2019-11-02 10:39:00 -0700

    Create Card, Deck, and suit enum

---
+++ Assets/Scripts/Deck.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck {
+
+    private List<Card> deck;
+    private static Random random = new Random();
+    
+    public void Shuffle() {
+        var count = deck.Count;
+        while (count > 1) {
+            count--;
+            var randomNext = Random.Range(0, count);
+            var card = deck[randomNext];
+            deck[randomNext] = deck[count];
+            deck[count] = card;
+        }
+    }
+
+    public Card Draw() {
+        Card drawnCard = deck[0];
+        deck.Remove(drawnCard);
+        return drawnCard;
+    }
+    
+    
+}
