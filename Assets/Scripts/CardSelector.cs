5ac909c84b508252eb24c9affd183b168f8c634c
Ishmael <jaswged@gmail.com>
2019-11-02 19:09:05 -0500

    Added state logic

--- Assets/Scripts/CardSelector.cs
+++ Assets/Scripts/CardSelector.cs
@@ -2,17 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CardSelector : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class CardSelector : MonoBehaviour {
+    public GameObject cardHighlightPrefab;
+    private GameObject cardHighlight;
+    public Material selectedMaterial;
+
+    public void EnterState() {
+        Debug.Log(("Enter select state"));
+        enabled = true;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit)) {
+            // Did the user click
+            if (Input.GetMouseButtonDown(0)) {
+                GameObject hitGo = hit.transform.gameObject;
+                if(hitGo.GetComponent<Card>() != null){
+                    Debug.Log("Clicked a card");
+                    // todo check if is your own card
+                    if (GameManagement.instance.DoesCardBelongToPlayerHand(hitGo)) {
+                        Debug.Log("Clicked own card");
+                        ExitState(hitGo);
+                    }
+                }
+            }
+        }
+    }
+    
+    private void ExitState(GameObject movingCard) {
+        if (GameManagement.instance.isBuryingCard) {
+            GameManagement.instance.isBuryingCard = false;
+            // TODO figure out how to pass which player.
+            GameManagement.instance.BuryCard(movingCard, false);
+        }
+        else {
+            this.enabled = false;
+            //TODO move the card to battle for the correct side.
+            GameManagement.instance.PlaceCard(movingCard, false);
+            
+            GameManagement.instance._battleState.EnterState(movingCard);
+        }
     }
 }
--- Assets/Scripts/PlayMat.cs
+++ Assets/Scripts/PlayMat.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
 
-public class PlayMat : MonoBehaviour
-{
+public class PlayMat : MonoBehaviour {
     [SerializeField]
     private GameObject cardPrefab;
     [SerializeField]
@@ -14,20 +13,36 @@ public class PlayMat : MonoBehaviour
     [SerializeField]
     private GameObject playMat;
 
-    private Deck redDeck;
-    private Deck blackDeck;
+    [SerializeField]
+    private GameObject redGraveyard;
+    [SerializeField]
+    private GameObject blackGraveyard;
+    [SerializeField]
+    private GameObject redBuryPit;
+    [SerializeField]
+    private GameObject blackberryPit;
+    [SerializeField]
+    private GameObject redBattlePosition;
+    [SerializeField]
+    private GameObject blackBattlePosition;
+    
+    public Deck redDeck;
+    public Deck blackDeck;
 
-    private Hand redHand;
-    private Hand blackHand;
+    public Hand redHand;
+    public Hand blackHand;
+
+    private Card redBuriedCard;
+    private Card blackberryCard;
     
     public void Initialize() {
         redDeck = SpawnDeck(true);
         blackDeck = SpawnDeck(false);
         
+        Debug.Log(("Draw the hand!")); 
         redHand = SpawnHand(true);
         blackHand = SpawnHand(false);
         
-        Debug.Log(("Draw the hand!"));
         DrawPlayerHand(true, true);
         DrawPlayerHand(false,true);
     }
@@ -48,21 +63,24 @@ public class PlayMat : MonoBehaviour
             
             // Reparent the card under the hand object.
             card.go.transform.parent = handToUse.go.transform;
+            card.go.transform.rotation = isRed ? Quaternion.Euler(-33.44f, 0f,0f) : Quaternion.Euler(33.44f, 0f,0f) ; 
             // TODO Taylor fix height problem
-            card.go.transform.position = handToUse.go.transform.position;
+            card.go.transform.position = handToUse.go.transform.position + (isRed ? new Vector3(-1.9f+(i*.75f), 0,0) : new Vector3(-1.9f+(i*.75f), 0,0));
+            card.go.transform.localScale -= new Vector3(.3f, .3f, 0); 
         }
     }
 
     private Hand SpawnHand(bool isRed) {
         // Spawn Hand
-        var handGo = Instantiate(handPrefab, (isRed ? new Vector3(-2, 0, -2) : new Vector3(5, 0, 5)),
-                                Quaternion.identity, playMat.transform);
+        var handGo = Instantiate(handPrefab, (isRed ? new Vector3(0, .91f, 3f) : new Vector3(0, .61f, -4.5f)),
+            Quaternion.identity, playMat.transform);
         handGo.name = isRed ? "RedHand" : "BlackHand";
         Hand hand = handGo.GetComponent<Hand>();
         hand.go = handGo;
 
         return hand;
     }
+
     private Deck SpawnDeck(bool isRed) {
         // TODO for reuse, pass in deck object?
         var deckGo = Instantiate(deckPrefab, playMat.transform);
@@ -77,8 +95,8 @@ public class PlayMat : MonoBehaviour
         
         for (int i = 2; i <= 14; i++) {
             var cardGo = GameObject.Instantiate(cardPrefab,
-                (isRed ? Vector3.zero : new Vector3(2, 0, 2)) + new Vector3(0, 0, i * .01f),
-                cardPrefab.transform.rotation, deckGo.transform);
+                    (isRed ? new Vector3(-4.01f, 0.1f-.3f, 3.5f) : new Vector3(3.87f, 0.1f-.3f, -3.8f)) + new Vector3(0, i * .05f, 0),
+                    cardPrefab.transform.rotation, deckGo.transform);
             Card card = cardGo.GetComponent<Card>();
             card.value = i;
             card.suit = isRed ? Suit.Diamonds : Suit.Clubs;
@@ -90,10 +108,11 @@ public class PlayMat : MonoBehaviour
 
             // Create second card for same value
             var cardGo2 = GameObject.Instantiate(cardPrefab,
-                (isRed ? Vector3.zero : new Vector3(2, 0, 2)) + new Vector3(0, 0, i * .01f),
+                (isRed ? new Vector3(-4.01f, 0.1f, 3.5f) : new Vector3(3.87f, 0.1f, -3.8f)) + new Vector3(0, cardGo.transform.position.y + .025f-0.09f, 0),
                 cardPrefab.transform.rotation, deckGo.transform);
             Card card2 = cardGo2.GetComponent<Card>();
             card2.value = i;
+            card2.go = cardGo2;
             card2.go = cardGo2;
             card2.suit = isRed ? Suit.Hearts : Suit.Spades;
             cardGo2.name = i + " of " + card2.suit;
@@ -102,5 +121,41 @@ public class PlayMat : MonoBehaviour
         }
 
         return deck;
+    }
+
+    public void BuryCard(GameObject movingCard, bool isRed) {
+        var handToRemoveFrom = isRed ? redHand : blackHand;
+        
+        movingCard.transform.position = isRed ? redBuryPit.transform.position : blackberryPit.transform.position;
+        movingCard.transform.parent = (isRed ? redBuryPit.transform : blackberryPit.transform);
+        // TODO @Taylor rotate back to flat
+        
+        var card = movingCard.GetComponent<Card>();
+        if (isRed) {
+            redBuriedCard = card;
+        }
+        else {
+            blackberryCard = card;
+        }
+
+        handToRemoveFrom.hand.Remove(card);
+    }
+
+    public void PlaceCard(GameObject movingCard, bool isRed) {
+        var handToRemoveFrom = isRed ? redHand : blackHand;
+        
+        var card = movingCard.GetComponent<Card>();
+        // TODO @Taylor rotate back to flat
+
+            movingCard.transform.position = (isRed ? redBattlePosition.transform.position: blackBattlePosition.transform.position);
+            movingCard.transform.parent = (isRed ? redBattlePosition.transform : blackBattlePosition.transform);
+
+            handToRemoveFrom.hand.Remove(card);
+    }
+
+    public void DiscardCard(GameObject blackCard, bool isRed) {
+        var graveToUse = isRed ? redGraveyard : blackGraveyard;
+        blackCard.transform.position = graveToUse.transform.position;
+        blackCard.transform.parent = graveToUse.transform;
     }
 }
